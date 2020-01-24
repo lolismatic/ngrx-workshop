fd8538fe001461a4145fb0955f5053c67def4561
Dan Patiu <dan.patiu@nordlogic.com>
2020-01-24 18:54:29 +0200

    ref(article-comment): use async pipe

--- src/app/features/article/components/article-comment.component.ts
+++ src/app/features/article/components/article-comment.component.ts
@@ -1,37 +1,24 @@
-import { Component, EventEmitter, Input, Output, OnInit, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 import { UserService } from '@app/services';
-import { Comment, User } from '@app/models';
+import { Comment } from '@app/models';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-article-comment',
   templateUrl: './article-comment.component.html',
 })
-export class ArticleCommentComponent implements OnInit, OnDestroy {
+export class ArticleCommentComponent {
+  canModify$ = this.userService.currentUser.pipe(
+    map((user) => user.username === this.comment.author.username)
+  );
+
   constructor(
     private userService: UserService
   ) {}
 
-  private subscription: Subscription;
-
   @Input() comment: Comment;
   @Output() deleteComment = new EventEmitter<boolean>();
-
-  canModify: boolean;
-
-  ngOnInit() {
-    // Load the current user's data
-    this.subscription = this.userService.currentUser.subscribe(
-      (userData: User) => {
-        this.canModify = (userData.username === this.comment.author.username);
-      }
-    );
-  }
-
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
-  }
 
   deleteClicked() {
     this.deleteComment.emit(true);
