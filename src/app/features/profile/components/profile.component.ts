acef9c1b48e0f231d6bf9bfb2cca0034f1a545a0
Dan Patiu <dan.patiu@nordlogic.com>
2020-01-24 19:24:57 +0200

    ref(profile-component): move isUser logic in resolver

--- src/app/features/profile/components/profile.component.ts
+++ src/app/features/profile/components/profile.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { concatMap ,  tap } from 'rxjs/operators';
 
-import { UserService } from '@app/services';
-import { User, Profile } from '@app/models';
+import { Profile } from '@app/models';
 
 @Component({
   selector: 'app-profile-page',
@@ -12,24 +10,14 @@ import { User, Profile } from '@app/models';
 export class ProfileComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
-    private userService: UserService
   ) { }
 
-  profile: Profile;
-  isUser: boolean;
+  profile: Profile & { isUser: boolean };
 
   ngOnInit() {
-    this.route.data.pipe(
-      concatMap((data: { profile: Profile }) => {
-        this.profile = data.profile;
-        // Load the current user's data.
-        return this.userService.currentUser.pipe(tap(
-          (userData: User) => {
-            this.isUser = (userData.username === this.profile.username);
-          }
-        ));
-      })
-    ).subscribe();
+    this.route.data.subscribe(
+      (data) => this.profile = data.profile
+    );
   }
 
   onToggleFollowing(following: boolean) {
--- src/app/features/profile/resolvers/profile.resolver.ts
+++ src/app/features/profile/resolvers/profile.resolver.ts
@@ -1,22 +1,32 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve, Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, withLatestFrom, map } from 'rxjs/operators';
 
-import { ProfilesService } from '@app/services';
+import { ProfilesService, UserService } from '@app/services';
 import { Profile } from '@app/models';
 
 @Injectable()
 export class ProfileResolver implements Resolve<Profile> {
   constructor(
     private profilesService: ProfilesService,
+    private userService: UserService,
     private router: Router
   ) {}
 
   resolve(route: ActivatedRouteSnapshot): Observable<any> {
-
-    return this.profilesService.get(route.params['username'])
-      .pipe(catchError((err) => this.router.navigateByUrl('/')));
+    return this.profilesService
+      .get(route.params['username'])
+      .pipe(
+        withLatestFrom(this.userService.currentUser),
+        map(([profile, user]) => {
+          return {
+            ...profile,
+            isUser: (profile.username === user.username)
+          };
+        }),
+        catchError((err) => this.router.navigateByUrl('/'))
+      );
 
   }
 }
